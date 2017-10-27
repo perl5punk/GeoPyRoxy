0954fdddbf47f0c428445d34f5380532fc31c4b2
Brian <brian@digitalpropulsionlabs.com>
2017-10-26 22:59:35 -0600

    setup config

---
+++ config.py
@@ -0,0 +1,12 @@
+
+class GeoPyRoxyConfig:
+
+    PREFERRED_SERVICE_ORDER = ['GoogleGeocoder', 'HereGeocoder']
+
+    GOOGLE_API_KEY = ''     # add your google api key here
+
+    HERE_APP_ID = ''        # add your here api key
+    HERE_APP_CODE = ''      # add your here app code
+
+
+
